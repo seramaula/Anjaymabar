76b4e2981795b6c0506f41ebcaefc8c54cdee31f
seramaula <85498983+seramaula@users.noreply.github.com>
2022-10-24 16:16:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.APIs = {
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-	'https://alphabot-api.herokuapp.com': 'V12',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://alphabot-api.herokuapp.com': 'cpZOFtZs',  // 👉 login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
